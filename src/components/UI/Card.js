c094fd0e1be36f6ded0009310c4b191413d2245d
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-11 10:59:55 +0700

    91. Adding a re-usable "Card" Component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Card.module.css";
+
+Card.propTypes = {};
+
+function Card(props) {
+  return (
+    <div className={`${classes.card} ${props.className}`}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Card;
